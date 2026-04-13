21c14879661789f8c953d29b81ceb41d601c3378
athulya24453 <e19328@eng.pdn.ac.lk>
2026-04-13 18:35:39 +0530

    added actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://athulya24453.github.io',
-  base: '/portfolio',
   vite: {
     cacheDir: '.vite-cache',
   },
